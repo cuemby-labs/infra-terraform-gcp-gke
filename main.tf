0d95e72684a300489c562fd9c58da96a045741c3
Manuel Nuila <manuel.nuila@cuemby.com>
2024-12-26 22:53:59 -0600

    first files

--- main.tf
+++ main.tf
@@ -1,3 +1,63 @@
+#
+# GKE
+#
+
+module "gke" {
+  source                     = "./modules/terraform-google-kubernetes-engine"
+  project_id                 = var.project_id
+  name                       = var.name
+  region                     = var.region
+  zones                      = var.zones
+  network                    = var.network
+  subnetwork                 = var.subnetwork
+  ip_range_pods              = var.ip_range_pods
+  ip_range_services          = var.ip_range_services
+  http_load_balancing        = var.http_load_balancing
+  network_policy             = var.network_policy
+  horizontal_pod_autoscaling = var.horizontal_pod_autoscaling
+  filestore_csi_driver       = var.filestore_csi_driver
+  dns_cache                  = var.dns_cache
+  node_pools                 = var.node_pools
+
+  # node_pools = [
+  #   {
+  #     name                        = var.node_pools["default"]["name"]
+  #     machine_type                = var.node_pools["default"]["machine_type"]
+  #     node_locations              = var.node_pools["default"]["node_locations"]
+  #     min_count                   = var.node_pools["default"]["min_count"]
+  #     max_count                   = var.node_pools["default"]["max_count"]
+  #     local_ssd_count             = var.node_pools["default"]["local_ssd_count"]
+  #     spot                        = var.node_pools["default"]["spot"]
+  #     disk_size_gb                = var.node_pools["default"]["disk_size_gb"]
+  #     disk_type                   = var.node_pools["default"]["disk_type"]
+  #     image_type                  = var.node_pools["default"]["image_type"]
+  #     enable_gcfs                 = var.node_pools["default"]["enable_gcfs"]
+  #     enable_gvnic                = var.node_pools["default"]["enable_gvnic"]
+  #     logging_variant             = var.node_pools["default"]["logging_variant"]
+  #     auto_repair                 = var.node_pools["default"]["auto_repair"]
+  #     auto_upgrade                = var.node_pools["default"]["auto_upgrade"]
+  #     service_account             = var.node_pools["default"]["service_account"]
+  #     preemptible                 = var.node_pools["default"]["preemptible"]
+  #     initial_node_count          = var.node_pools["default"]["initial_node_count"]
+  #     accelerator_count           = var.node_pools["default"]["accelerator_count"]
+  #     accelerator_type            = var.node_pools["default"]["accelerator_type"]
+  #     gpu_driver_version          = var.node_pools["default"]["gpu_driver_version"]
+  #     gpu_sharing_strategy        = var.node_pools["default"]["gpu_sharing_strategy"]
+  #     max_shared_clients_per_gpu  = var.node_pools["default"]["max_shared_clients_per_gpu"]
+  #   },
+  # ]
+
+  node_pools_oauth_scopes = var.node_pools_oauth_scopes
+  node_pools_labels       = var.node_pools_labels
+  node_pools_metadata     = var.node_pools_metadata
+  node_pools_taints       = var.node_pools_taints
+  node_pools_tags         = var.node_pools_tags
+}
+
+#
+# Walrus Resources
+#
+
 locals {
   context = var.context
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,174 @@
 #
+# GKE Variables
+#
+
+variable "project_id" {
+  description = "The project ID to host the cluster in (required)"
+  type        = string
+  default     = ""
+}
+
+variable "name" {
+  description = "The name of the cluster (required)"
+  type        = string
+  default     = "gke-cluster-"
+}
+
+variable "region" {
+  description = "The region to host the cluster in (optional if zonal cluster / required if regional)"
+  type        = string
+  default     = null
+}
+
+variable "zones" {
+  description = "The zones to host the cluster in (optional if regional cluster / required if zonal)"
+  type        = list(string)
+  default     = []
+}
+
+variable "network" {
+  description = "The VPC network to host the cluster in (required)."
+  type        = string
+  default     = ""
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to host the cluster in (required)."
+  type        = string
+  default     = ""
+}
+
+variable "ip_range_pods" {
+  description = "The _name_ of the secondary subnet ip range to use for pods"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ip_range_services" {
+  description = "The _name_ of the secondary subnet range to use for services"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "http_load_balancing" {
+  description = "Enable httpload balancer addon"
+  type        = bool
+  default     = true
+}
+
+variable "network_policy" {
+  description = "Enable network policy addon"
+  type        = bool
+  default     = false
+}
+
+variable "horizontal_pod_autoscaling" {
+  description = "Enable or disable horizontal pod autoscaling."
+  type        = bool
+  default     = true
+}
+
+variable "filestore_csi_driver" {
+  description = "The status of the Filestore CSI driver addon, which allows the usage of filestore instance as volumes"
+  type        = bool
+  default     = false
+}
+
+variable "dns_cache" {
+  description = "The status of the NodeLocal DNSCache addon."
+  type        = bool
+  default     = false
+}
+
+variable "node_pools" {
+  type        = list(map(any))
+  description = "List of maps containing node pools"
+
+  default = [
+    {
+      name                        = "default-node-pool"
+      machine_type                = "e2-medium"
+      node_locations              = "us-central1-b,us-central1-c"
+      min_count                   = 1
+      max_count                   = 100
+      local_ssd_count             = 0
+      spot                        = false
+      disk_size_gb                = 100
+      disk_type                   = "pd-standard"
+      image_type                  = "COS_CONTAINERD"
+      enable_gcfs                 = false
+      enable_gvnic                = false
+      logging_variant             = "DEFAULT"
+      auto_repair                 = true
+      auto_upgrade                = true
+      service_account             = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
+      preemptible                 = false
+      initial_node_count          = 80
+      accelerator_count           = 1
+      accelerator_type            = "nvidia-l4"
+      gpu_driver_version          = "LATEST"
+      gpu_sharing_strategy        = "TIME_SHARING"
+      max_shared_clients_per_gpu  = 2
+    }
+  ]
+}
+
+variable "node_pools_oauth_scopes" {
+  type        = map(list(string))
+  description = "Map of lists containing node oauth scopes by node-pool name"
+
+  # Default is being set in variables_defaults.tf
+  default = {
+    all               = ["https://www.googleapis.com/auth/cloud-platform"]
+    default-node-pool = []
+  }
+}
+
+variable "node_pools_labels" {
+  type        = map(map(string))
+  description = "Map of maps containing node labels by node-pool name"
+
+  # Default is being set in variables_defaults.tf
+  default = {
+    all               = {}
+    default-node-pool = {}
+  }
+}
+
+variable "node_pools_metadata" {
+  type        = map(map(string))
+  description = "Map of maps containing node metadata by node-pool name"
+
+  # Default is being set in variables_defaults.tf
+  default = {
+    all               = {}
+    default-node-pool = {}
+  }
+}
+
+variable "node_pools_taints" {
+  type        = map(list(object({ key = string, value = string, effect = string })))
+  description = "Map of lists containing node taints by node-pool name"
+
+  # Default is being set in variables_defaults.tf
+  default = {
+    all               = []
+    default-node-pool = []
+  }
+}
+
+variable "node_pools_tags" {
+  type        = map(list(string))
+  description = "Map of lists containing node network tags by node-pool name"
+
+  # Default is being set in variables_defaults.tf
+  default = {
+    all               = []
+    default-node-pool = []
+  }
+}
+
+#
 # Contextual Fields
 #
 
