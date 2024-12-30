11fb36047440031de95c393c17969dddb3c63dfd
Manuel Nuila <manuel.nuila@cuemby.com>
2024-12-30 12:56:30 -0600

    update provider version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.5.7"
+  required_version = ">= 1.5.7"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=6.10.0"
+      version = ">= 6.10.0"
     }
   }
 }
